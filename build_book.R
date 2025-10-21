8f2a3569d0949aa9bc5d8ecf8db4183e8e01ca68
j-baldock <jbaldock@usgs.gov>
2025-10-21 12:17:20 -0600

    initial commit

---
+++ build_book.R
@@ -0,0 +1,8 @@
+library(quarto)
+library(knitr)
+
+# render whole book
+quarto::quarto_render(output_format = "html")
+
+# render single chapters
+quarto::quarto_render("index.qmd", output_format = "html")
