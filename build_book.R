d83816eaf47b989546fa027ed147f99df77cafed
j-baldock <jbaldock@usgs.gov>
2025-10-21 18:08:06 -0600

    update

--- build_book.R
+++ build_book.R
@@ -6,3 +6,4 @@ quarto::quarto_render(output_format = "html")
 
 # render single chapters
 quarto::quarto_render("index.qmd", output_format = "html")
+quarto::quarto_render("exploredata.qmd", output_format = "html")
